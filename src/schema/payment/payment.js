198f9df41b151e9ddb7988f7274ae95f1614aebd
Darshit Karkar <99006780+darshitkarkar101@users.noreply.github.com>
2024-06-18 01:46:59 -0600

    feat: Add payment API endpoints, schema, and routes

---
+++ src/schema/payment/payment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const enums = require("../../../json/enums.json");
+
+module.exports = (connection) => {
+  const paymentSchema = new mongoose.Schema(
+    {
+      uid: { type: mongoose.Schema.Types.ObjectId, ref: "admin" },
+      cid: { type: mongoose.Schema.Types.ObjectId, ref: "cart" },
+      amount : { type: Number },
+      number : { type: String },
+      status : { type: String },
+      paymentId : { type: String },
+    },
+    {
+      autoCreate: true,
+      timestamps: true,
+    }
+  );
+
+  // return logsSchema;
+  return connection.model("payment", paymentSchema, "payment");
+};
