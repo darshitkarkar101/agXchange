7c20832f585a5aa55e1e7e77adfeee9ceaf44911
Darshit Karkar <99006780+darshitkarkar101@users.noreply.github.com>
2024-06-18 01:47:41 -0600

    chore: Add API endpoints, schemas, and routes for plans and products

---
+++ src/api/plans/index.js
@@ -0,0 +1,11 @@
+const addPlans = require("./create-plans");
+const getPlans = require("./get-plans");
+const updatePlans = require("./update-plans");
+const deletePlans = require("./delete-plans");
+
+module.exports = exports = {
+  addPlans,
+  getPlans,
+  updatePlans,
+  deletePlans
+};
